c2fc2290dc400eefc84e275c8f7520600615949f
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-08-30 15:58:57 +0900

    hooks

---
+++ src/components/common/list/hooks/useList.ts
@@ -0,0 +1,43 @@
+import { useCallback, useEffect, useState } from 'react';
+import { fakerType } from '../index';
+import faker from 'faker';
+
+const useList = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [target, setTarget] = useState<HTMLDivElement | null>(null);
+  const [itemLists, setItemLists] = useState<fakerType[] | []>([]);
+
+  const getMoreItem = useCallback(async () => {
+    setIsLoaded(true);
+    await new Promise((resolve) => setTimeout(resolve, 800));
+    let Items: fakerType[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => {
+      return { name: faker.lorem.word(), writing: faker.lorem.text() };
+    });
+
+    setItemLists((itemLists: fakerType[]) => itemLists.concat(Items));
+    setIsLoaded(false);
+  }, [itemLists]);
+
+  const onIntersect = useCallback(async ([entry]: any, observer: any) => {
+    if (entry.isIntersecting && !isLoaded) {
+      observer.unobserve(entry.target);
+      await getMoreItem();
+      observer.observe(entry.target);
+    }
+  }, []);
+
+  useEffect(() => {
+    let observer: any;
+    if (target) {
+      observer = new IntersectionObserver(onIntersect, {
+        threshold: 0.4,
+      });
+      observer.observe(target);
+    }
+    return () => observer && observer.disconnect();
+  }, [target]);
+
+  return { itemLists, isLoaded, setTarget };
+};
+
+export default useList;
--- src/components/common/list/index.tsx
+++ src/components/common/list/index.tsx
@@ -1,47 +1,15 @@
-import React, { memo, useCallback, useEffect, useState } from 'react';
-import faker from 'faker';
+import React, { memo } from 'react';
 import { Container, ItemContainer, ItemName, ItemWriting } from './List';
 import Loader from './Loading';
+import useList from './hooks/useList';
 
-interface fakerType {
+export interface fakerType {
   name: string | null;
   writing: string | null;
 }
 
 const List = () => {
-  const [target, setTarget] = useState<HTMLDivElement | null>(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [itemLists, setItemLists] = useState<fakerType[] | []>([]);
-
-  const getMoreItem = useCallback(async () => {
-    setIsLoaded(true);
-    await new Promise((resolve) => setTimeout(resolve, 800));
-    let Items: fakerType[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => {
-      return { name: faker.lorem.word(), writing: faker.lorem.text() };
-    });
-
-    setItemLists((itemLists: fakerType[]) => itemLists.concat(Items));
-    setIsLoaded(false);
-  }, [itemLists]);
-
-  const onIntersect = useCallback(async ([entry]: any, observer: any) => {
-    if (entry.isIntersecting && !isLoaded) {
-      observer.unobserve(entry.target);
-      await getMoreItem();
-      observer.observe(entry.target);
-    }
-  }, []);
-
-  useEffect(() => {
-    let observer: any;
-    if (target) {
-      observer = new IntersectionObserver(onIntersect, {
-        threshold: 0.4,
-      });
-      observer.observe(target);
-    }
-    return () => observer && observer.disconnect();
-  }, [target]);
+  const { isLoaded, itemLists, setTarget } = useList();
 
   return (
     <Container>
